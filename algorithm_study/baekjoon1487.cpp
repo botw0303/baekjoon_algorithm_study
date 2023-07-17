3eb3801ca422bcbcc45c6ad329655c5072bb32d7
botw0303 <zeldabotw0303@gmail.com>
2023-07-17 19:09:05 +0900

    baekjoon1487 finished

--- algorithm_study/baekjoon1487.cpp
+++ algorithm_study/baekjoon1487.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr); cout.tie(nullptr);
-	int n, maxProfit = 0, answer = 0;
+	long long n, maxProfit = 0, answer = 0;
 	vector<pair<int, int>> v;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
@@ -14,11 +14,12 @@ int main() {
 		cin >> price >> dc;
 		v.push_back(make_pair(price, dc));
 	}
+	sort(v.begin(), v.end());
 	for (int i = 0; i < n; i++) {
 		int price = v[i].first;
 		int profit = 0;
 		for (int j = 0; j < n; j++) {
-			if (v[j].first >= price) profit += v[i].first - v[i].second;
+			if (v[j].first >= price && v[i].first - v[j].second > 0) profit += v[i].first - v[j].second;
 		}
 		if (profit > maxProfit) {
 			maxProfit = profit;
