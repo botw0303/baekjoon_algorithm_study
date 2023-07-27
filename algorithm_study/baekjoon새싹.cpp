1a4c3770b041801271b07db81b1b5d27872b599c
botw0303 <zeldabotw0303@gmail.com>
2023-07-27 18:13:15 +0900

    새싹 문제 오늘로 3일차 75% 풀었음

--- algorithm_study/baekjoon새싹.cpp
+++ algorithm_study/baekjoon새싹.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-	int a, b;
-	vector<int> v;
-	while (cin >> a >> b) {
-		cout << a + b << "\n";
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	vector<int> v, answer;
+	for (int i = 0; i < 30; i++) {
+		answer.push_back(i + 1);
+	}
+	for (int i = 0; i < 28; i++) {
+		int input;
+		cin >> input;
+		for (int j = 0; j < answer.size(); j++) {
+			if (answer[j] == input) answer.erase(answer.begin() + j);
+		}
+	}
+	for (int elem : answer) {
+		cout << elem << "\n";
 	}
 }
