77d2b3141141cc45e28cac8783033f1de0d8535b
botw0303 <zeldabotw0303@gmail.com>
2024-07-17 23:48:16 +0900

    more

--- algorithm_study/baekjoon1018.cpp
+++ algorithm_study/baekjoon1018.cpp
@@ -1,29 +1,76 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
 using namespace std;
+string WB[8] = {
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW"
+};
+string BW[8] = {
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB"
+};
+string board[50];
+int WB_cnt(int x, int y)
+{
+    int cnt = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (board[x + i][y + j] != WB[i][j])
+                cnt++;
+        }
 
+    }
+    return cnt;
+}
+int BW_cnt(int x, int y)
+{
+    int cnt = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (board[x + i][y + j] != BW[i][j])
+                cnt++;
+        }
+
+    }
+    return cnt;
+}
 int main() {
-	int n, m, cnt = 0;
-	cin >> n >> m;
-	bool** inputBoard = new bool*[n];
-	for (int i = 0; i < n; i++) {
-		inputBoard[i] = new bool[m];
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			char input;
-			cin >> input;
-			if (input == 'B') {
-				inputBoard[i][j] = true;
-			}
-			else {
-				inputBoard[i][j] = false;
-			}
-		}
-	}
-	for (int i = 0; i <= n - 8; i++) {
-		for (int j = 0; j <= m - 8; j++) {
-
-		}
-	}
-	cout << cnt;
+    int size[2];
+    int cnt;
+    int min_val = 12345;
+    pair<int, int> p1;
+    cin >> p1.first >> p1.second;
+    for (int i = 0; i < p1.first; i++)
+        cin >> board[i];
+    for (int i = 0; i + 8 <= p1.first; i++)
+    {
+        for (int j = 0; j + 8 <= p1.second; j++)
+        {
+            int tmp;
+            tmp = min(WB_cnt(i, j), BW_cnt(i, j));
+            if (tmp < min_val) {
+                min_val = tmp;
+            }
+        }
+    }
+    cout << min_val;
+    return 0;
 }
