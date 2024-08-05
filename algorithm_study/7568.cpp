ac3b90fe5c603d2ad816e8a2d2ddbfbef8771258
botw0303 <zeldabotw0303@gmail.com>
2024-08-05 09:59:41 +0900

    백준 7568 덩치

--- algorithm_study/7568.cpp
+++ algorithm_study/7568.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
-#include <deque>
+#include <vector>
 
 using namespace std;
 
 int main() {
-	deque<pair<int, int>> dq;
+	vector<pair<int, int>> vec;
+	vector<int> answer;
 	int n = 0;
 	cin >> n;
 	for (int i = 0; i < n; ++i)
 	{
 		int x, y;
 		cin >> x >> y;
-		dq.push_back(make_pair(x, y));
+		vec.push_back(make_pair(x, y));
 	}
 
+	for (int i = 0; i < n; ++i)
+	{
+		answer.push_back(1);
+		for (int j = 0; j < n; ++j)
+		{
+			if (vec[i].first < vec[j].first && vec[i].second < vec[j].second)
+				answer[i] += 1;
+		}
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		cout << answer[i] << " ";
+	}
 }
